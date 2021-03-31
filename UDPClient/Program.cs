db30e95fad3a2e35d7b743ad8f22909168206596
Rostyslav Hetmaniuk <hetmaniuk.rostyslav@gmail.com>
2021-03-31 18:34:50 +0300

    add test sender

---
+++ UDPClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new UdpClientManager("127.0.0.1", 20000);
+
+            Task.Factory.StartNew(async () => {
+                while (true)
+                {
+                    try
+                    {
+                        client.Send($"Test message + {DateTime.Now.ToShortTimeString()}");
+                        await Task.Delay(1000);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UDPClient/UdpClientManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPClient
+{
+    public class UdpClientManager
+    {
+        private readonly UdpClient _client;
+
+        public UdpClientManager(string hostname, int port)
+        {
+            _client = new UdpClient();
+            _client.Connect(hostname, port);
+        }
+
+        public void Send(string message)
+        {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            _client.Send(datagram, datagram.Length);
+        }
+    }
+}
