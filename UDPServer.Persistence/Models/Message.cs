e9aad70bc50b3d1cda25fa577b9f45ce57631cf2
Rostyslav Hetmaniuk <hetmaniuk.rostyslav@gmail.com>
2021-03-31 10:51:44 +0300

    update message

--- UDPServer.Persistence/Models/Message.cs
+++ UDPServer.Persistence/Models/Message.cs
@@ -5,7 +5,6 @@ namespace UDPServer.Persistence.Models
     public class Message : Entity
     {
         public string Text { get; set; }
-        public string SenderId { get; set; }
         public Sender Sender { get; set; }
         public DateTime CreatedAt { get; set; }
     }
